05bffdc1b0941db22bbff29426ba523e999c8325
dekhte hain <divyasharmagdscoist@gmail.com>
2025-08-02 13:55:07 +0530

    fix: conditional app.run for local vs Render

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -98,6 +98,5 @@ if __name__ == "__main__":
     # Initial data load
     data_fetcher.fetch_latest_data()
     print("app.py ran successfully")
-    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or fallback to 5000
-    app.run(host='0.0.0.0', port=port)
-
+    #if os.environ.get("RENDER") != "true":
+    #  app.run(host='0.0.0.0', port=port)
