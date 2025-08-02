ccf04207b6b28e8972817c9aacdd6a86d76832de
dekhte hain <divyasharmagdscoist@gmail.com>
2025-08-02 20:37:50 +0530

    changes in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects', // ← this is your source
-          dest: '.'                 // ← this means "copy to dist/"
-        }
-      ]
-    })
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
   },
