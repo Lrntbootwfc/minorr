acaf70d4049c17770e35558c4b91c425ac9c4b66
dekhte hain <divyasharmagdscoist@gmail.com>
2025-08-02 20:16:18 +0530

    paths

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', // ← this is your source
+          dest: '.'                 // ← this means "copy to dist/"
+        }
+      ]
+    })
+  ],
   build: {
     outDir: 'dist',
   },
